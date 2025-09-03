a85894030d49fa7aea34d3fa800b3160ce408603
Rupesh Singh Rawat <rupeshrawat133@gmail.com>
2025-09-04 02:01:23 +0530

    your update message here

--- server/server.js
+++ server/server.js
@@ -63,7 +63,12 @@ try {
 }
 
 // Start server
+if(process.env.NODE_ENV !== "production" ){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
   console.log(`🚀 Server is running on PORT: ${PORT}`)
 );
+}
+//export server for vercel
+
+export default server;
